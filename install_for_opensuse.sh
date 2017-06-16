18a087115559b9d3164177b8d0407c303167b0da
samil goh <iam312@gmail.com>
2017-06-17 00:42:19 +0900

    opensuse 용 패키지 추가

--- install_for_opensuse.sh
+++ install_for_opensuse.sh
@@ -1,5 +1,5 @@
 # install git, tmux, make, gcc
-sudo zypper install -y git-core tmux make gcc gcc-c++
+sudo zypper install -y  git-core tmux zsh vim vim-data wget tar make gcc gcc-c++ ruby zlib-devel libopenssl-devel ncurses-utils glibc-locale youtube-dl readline-devel libbz2-devel libsqlite3-0 sqlite3-devel
 
 # copy dot files
 cp -r .bashrc .gitignore .matplotlib .tmux.conf .vimrc .zshrc ~/
