fc24d4e3e6afbcdacaccf44a73132c32ff815fc4
Samil Goh <iam312@gmail.com>
2025-12-28 11:53:02 +0900

    add missing packages for opensuse

--- install_for_wsl_opensuse_ml.zsh
+++ install_for_wsl_opensuse_ml.zsh
@@ -14,7 +14,7 @@ if [ -z "$ZSH_VERSION" ]; then
 fi
 
 # install git, tmux, make, gcc
-sudo zypper install -y  git-core tmux zsh vim vim-data wget curl curl-zsh-completion tar make gcc gcc-c++ ruby zlib-devel libopenssl-devel ncurses-utils glibc-locale youtube-dl readline-devel libbz2-devel libsqlite3-0 sqlite3-devel
+sudo zypper install -y  git-core tmux zsh vim vim-data wget curl curl-zsh-completion tar make gcc gcc-c++ ruby zlib-devel libopenssl-devel ncurses-utils glibc-locale youtube-dl readline-devel libbz2-devel libsqlite3-0 sqlite3-devel graphviz baekmuk-ttf-fonts
 
 ./install_tools_for_ml.zsh
 
